9318bc24448511c4adfe8f08e97ef9900daa43de
Matt Carter <m@ttcarter.com>
2019-11-04 10:09:14 +0100

    BUGFIX: More MacGyver/Node fixes REFACTOR: Moved Vue specific functionality into its own file

---
+++ src/entrypoint-node.js
@@ -0,0 +1,2 @@
+import MacGyver from './macgyver.vue';
+export default MacGyver;
--- src/macgyver.vue
+++ src/macgyver.vue
@@ -8,11 +8,58 @@ import Vue from 'vue';
 * @var {Object}
 */
 Vue.prototype.$macgyver = (()=> {
+	var $macgyver = MacGyver;
+
 	// Absorb various methods from a Vue prototype
 	var vInstance = new Vue();
-	['$on', '$off', '$once', '$emit'].forEach(method => MacGyver[method] = vInstance[method].bind(vInstance));
+	['$on', '$off', '$once', '$emit'].forEach(method => $macgyver[method] = vInstance[method].bind(vInstance));
 
-	return MacGyver;
+	/**
+	* Glue various MacGyver emitter handlers to a registered component
+	* Registers a few universal handles, which can be called via $macgyver.forms.emitDown(id, EVENT):
+	*   - mgIdentify - component will respond with its Vue instance
+	*   - mgRefresh - component with refresh its data state
+	*
+	* Adds a watcher which will emit `mgChange` when the `data` property changes (basically transforms the change event to `mgChange`)
+	*
+	* @param {VueInstance} component The component to register against
+	*/
+	$macgyver.inject = component => {
+		// Sanity checks {{{
+		if (!component.$props.form) throw new Error('Cannot locate $props.form <string>, make sure that vm{props: {form: String, config: Object}} is declared');
+		// }}}
+
+		component.$on('mgIdentify', reply => reply(component));
+
+		// Read in initial data value
+		if (component.$props.config.$dataPath) {
+			var refresher = ()=> {
+				component.data = _.get($macgyver.$forms[component.$props.form].$props.data, component.$props.config.$dataPath);
+			};
+
+			component.$on('mgRefresh', refresher);
+			refresher();
+		}
+
+		// Inject data watcher which transforms change operations into emitters to the nearest parent form {{{
+		component.$watch('data', val => {
+			$macgyver.$forms[component.$props.form].$emit('mgChange', component.$props.config.$dataPath, val);
+		});
+		// }}}
+	};
+
+
+	/**
+	* Registeres a MacGyver form by its generated ID
+	* @params {VueInstance} component The Vue component to register
+	*/
+	$macgyver.injectForm = component => {
+		$macgyver.$forms[component.id] = component;
+		component.$on('mgIdentify', reply => reply(component));
+	};
+
+
+	return $macgyver;
 })();
 
 export default Vue.prototype.$macgyver;
