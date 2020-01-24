fcb2258519a8b2c4516c0063bf5f50b06cc511a0
Matt Carter <m@ttcarter.com>
2020-01-24 13:15:39 +1000

    TEST: Misc test fixes

--- test/node.js
+++ test/node.js
@@ -9,7 +9,7 @@ describe('Basic sanity checks', ()=> {
 		expect(macgyver.widgets).to.be.an('object');
 
 		[
-			'fetch', 'flatten', 'neatenSpec', 'register',
+			'fetch', 'flatten', 'compileSpec', 'register',
 			'forms.setConfig', 'forms.setData', 'forms.validate', 'forms.getPrototype', 'forms.run'
 		].forEach(p => {
 			expect(macgyver).to.have.nested.property(p);
