0302679f3361616e85d654bf782f3ae4d2be9a41
Matt Carter <m@ttcarter.com>
2020-01-26 11:29:46 +1000

    Added mgCodeEditor

---
+++ src/components/mgCodeEditor.vue
@@ -0,0 +1,84 @@
+<script>
+macgyver.register('mgCodeEditor', {
+	requires: 'node_modules/ace-builds/src-noconflict/ace.js',
+	title: 'Code Editor',
+	icon: 'fal fa-code',
+	category: 'Complex Inputs',
+	preferId: true,
+	config: {
+		syntax: {type: 'mgChoiceDropdown', enum: ['text', 'json', 'javascript'], default: 'json'},
+		convert: {type: 'mgToggle', default: true, showIf: 'syntax == "json"', help: 'Convert data back to a native JS object'},
+		theme: {type: 'mgChoiceDropdown', enum: ['chrome'], advanced: true, default: 'chrome', help: 'The syntax color scheme to use'},
+		height: {type: 'mgText', default: '400px', help: 'The size of the editing window as a valid CSS measurement', advanced: true},
+	},
+});
+
+export default Vue.component('mgCodeEditor', {
+	data: ()=> ({
+		data: undefined,
+	}),
+	props: {
+		config: Object,
+		form: String,
+	},
+	watch: {
+		config() {
+			this.editor.getSession().setMode(`ace/mode/${this.$props.config.syntax}`);
+			this.editor.setTheme(`ace/theme/${this.$props.config.theme}`);
+		},
+	},
+	created() {
+		this.$macgyver.inject(this);
+	},
+	beforeDestroy() {
+		this.editor.destroy();
+		this.editor.container.remove();
+	},
+	mounted() {
+		this.editor = ace.edit(this.$el);
+		this.editor.$blockScrolling = Infinity;
+
+		this.editor.setValue(
+			_.isObject(this.data) ? JSON.stringify(this.data, null, '\t') // Parse raw objects into JSON
+			: this.data ? this.data
+			: ''
+		, 1);
+
+		this.editor.setOptions({
+			showPrintMargin: false,
+		});
+
+		this.editor.on('change', ()=> {
+			var val = this.editor.getValue();
+			if (this.$props.config.convert && this.$props.config.syntax == 'json') {
+				try {
+					val = JSON.parse(val);
+					this.$macgyver.forms.emit(this.form, 'mgChange', this.$props.config.id, val)
+				} catch (e) {
+					// Silently fail as the JSON is invalid
+				}
+			} else {
+				this.$macgyver.forms.emit(this.form, 'mgChange', this.$props.config.id, val)
+			}
+			return true;
+		});
+
+		this.$nextTick(()=> this.editor.resize());
+	},
+	render(h) {
+		return h('div', {
+			attrs: {
+				class: 'mg-code-editor',
+				style: `height: ${this.$props.config.height}; width: 100%`,
+			},
+		});
+	},
+});
+</script>
+
+<style>
+.mg-code-editor {
+	border: 1px solid #f0f0f0;
+	border-radius: 5px;
+}
+</style>
