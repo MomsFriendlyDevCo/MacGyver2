a8c03a61e9b2ce20fe10784c94d9a74932bb1083
Mr-Yellow <mr-yellow@mr-yellow.com>
2020-12-20 10:40:57 +1100

    Migrated to VJsoneditor

--- src/components/mgCode.vue
+++ src/components/mgCode.vue
@@ -1,4 +1,6 @@
 <script>
+import VJsoneditor from 'v-jsoneditor';
+
 export default Vue.mgComponent('mgCode', {
 	meta: {
 		title: 'Code Editor',
@@ -6,89 +8,47 @@ export default Vue.mgComponent('mgCode', {
 		category: 'Complex Inputs',
 		preferId: true,
 	},
+	components: {VJsoneditor},
+	data() { return {
+		options: {},
+	}},
 	props: {
+		enableSort: {type: 'mgChoiceCheckbox', default: true},
+		mode: {type: 'mgChoiceDropdown', enum: ['view', 'form', 'code', 'text', 'preview'], default: 'tree'},
+		modes: {type: 'mgChoiceList', default: ['tree', 'code']},
+		search: {type: 'mgChoiceCheckbox', default: true},
 		syntax: {type: 'mgChoiceDropdown', enum: ['text', 'json', 'javascript', 'html', 'css'], default: 'json'},
-		convert: {type: 'mgToggle', default: true, showIf: 'syntax == "json"', help: 'Convert data back to a native JS object'},
 		theme: {type: 'mgChoiceDropdown', enum: ['chrome'], advanced: true, default: 'chrome', help: 'The syntax color scheme to use'},
 		height: {type: 'mgText', default: '400px', help: 'The size of the editing window as a valid CSS measurement', advanced: true},
 	},
-	beforeDestroy() {
-		this.editor.destroy();
-		this.editor.container.remove();
+	methods: {
+		errorHandler(e) {
+			this.$debug('errorHandler', e);
+		}
 	},
-	mounted() {
-		this.editor = ace.edit(this.$el);
-		this.editor.$blockScrolling = Infinity;
+	created() {
+		this.$debugging = false;
 
-		this.editor.setOptions({
-			showPrintMargin: false,
-		});
-
-		this.editor.on('change', (delta)=> {
-			var value = this.editor.getValue();
-			if (this.$props.convert && this.$props.syntax == 'json') {
-				try {
-					value = JSON.parse(value);
-
-					/*
-					// Replace dollarsign prefixed items with Unicode
-					// https://stackoverflow.com/a/39126851/2438830
-					function replaceKeysDeep(obj) {
-						return _.transform(obj, function(res, v, k) {
-							var cur = _.isString(k) ? k.replace(/^\$/, '\uFF04') : k;
-							res[cur] = _.isObject(v) ? replaceKeysDeep(v) : v;
-						});
-					}
-					replaceKeysDeep(value);
-					*/
-
-					this.data = value;
-				} catch (e) {
-					// Silently fail as the JSON is invalid
-					console.log('Invalid JSON', e);
-				}
-			} else {
-				this.data = value;
-			}
-			return true;
-		});
-
-		this.$nextTick(()=> this.editor.resize());
-
-		/*
-		this.$watch('config', ()=> {
-			// TODO: Make compatible with Parcel
-			//if (this.$props.syntax) this.editor.getSession().setMode(`ace/mode/${this.$props.syntax}`);
-			//if (this.$props.theme) this.editor.setTheme(`ace/theme/${this.$props.theme}`);
-		}, {
-			// FIXME: deep?
-			immediate: true
-		});
-		*/
-
-		this.$watch('data', (newVal, oldVal)=> {
-			if (!newVal) return;
-			var value = this.editor.getValue();
-
-			if (this.$props.convert && this.$props.syntax == 'json')
-				newVal = JSON.stringify(newVal, null, '\t');
-
-			// FIXME: This comparison will fail with parsed (convert = true) instances, resulting in update loop.
-			if (newVal === value) return;
-
-			this.editor.setValue(newVal, 1);
-		}, {deep: true, immediate: true});
-	},
-	render(h) {
-		return h('div', {
-			attrs: {
-				class: 'mg-code',
-				style: `height: ${this.$props.height}; width: 100%`,
-			},
-		});
+		this.options.enableSort = this.enableSort;
+		this.options.mode = this.mode;
+		this.options.modes = this.modes;
+		this.options.search = this.search;
+		this.options.theme = this.theme;
 	},
 });
 </script>
+
+<template>
+	<div class="mg-code">
+		<v-jsoneditor
+			v-model="data"
+			:options="options"
+			:plus="false"
+			:height="height"
+			@error="errorHandler"
+		/>
+	</div>
+</template>
 
 <style>
 .mg-code {
