7ef335e377a957304874c59383fa1ee7c7364357
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-02-09 15:20:13 +1100

    BUGFIX: $setPath root object creation

--- src/services/setPath.js
+++ src/services/setPath.js
@@ -19,54 +19,56 @@
 * vm.$setPath('foo.1.bar', 123); // vm.$data.foo = [{bar: 123}]
 */
 export default {
-	install: function(Vue, options) {
-		console.log('$setPath install', _.has(Vue, '$setPath'));
-		console.log('FIXME')
-		return;
-		Vue.prototype.$setPath = function(target, path, value, options) {
-			// Argument mangling {{{
-			if (_.isString(target) || _.isArray(target) || value === undefined) { // called as (path, value)
-				[target, path, value, options] = [this, target, path, value];
-			} else if (!_.isObject(target)) {
-				throw new Error('Cannot use $setPath on non-object target');
-			}
-			// }}}
-
-			var settings = {
-				arrayNumeric: true,
-				debug: false,
-				removeUndefined: true,
-				...options,
-			};
-
-			if (settings.debug) console.log('[$setPath]', path, '=', value, {target, options});
-
-			var node = target;
-			if (!path) throw new Error('Cannot $setPath with undefined path');
-			(_.isString(path) ? path.split('.') : path).some((chunk, chunkIndex, chunks) => {
-				if (chunkIndex == chunks.length - 1) { // Leaf node
-					if (settings.removeUndefined && value === undefined) {
-						this.$unset(node, chunk);
-					} else {
-						this.$set(node, chunk, value);
+	install: function(app, options) {
+		app.mixin({
+			methods: {
+				$setPath: function(target, path, value, options) {
+					// Argument mangling {{{
+					if (_.isString(target) || _.isArray(target) || value === undefined) { // called as (path, value)
+						[target, path, value, options] = [this, target, path, value];
+					} else if (!_.isObject(target)) {
+						throw new Error('Cannot use $setPath on non-object target');
 					}
-				} else if (node[chunk] === undefined) { // This chunk (and all following chunks) does't exist - populate from here
-					chunks.slice(chunkIndex).forEach(chunk => {
-						if (settings.arrayNumeric && isFinite(chunk)) {
-							this.$set(node, chunk, []);
-						} else {
-							this.$set(node, chunk, {});
-						}
-						node = node[chunk];
-					});
-					return true;
-				} else {
-					node = node[chunk];
-					return false;
-				}
-			});
+					// }}}
 
-			return value;
-		};
+					var settings = {
+						arrayNumeric: true,
+						debug: false,
+						removeUndefined: true,
+						...options,
+					};
+
+					if (settings.debug) console.log('[$setPath]', path, '=', value, {target, options});
+
+					var node = target;
+					if (!path) throw new Error('Cannot $setPath with undefined path');
+					(_.isString(path) ? path.split('.') : path).some((chunk, chunkIndex, chunks) => {
+						if (chunkIndex == chunks.length - 1) { // Leaf node
+							if (settings.removeUndefined && value === undefined) {
+								this.$unset(node, chunk);
+							} else {
+								this.$set(node, chunk, value);
+							}
+						} else if (node[chunk] === undefined) { // This chunk (and all following chunks) does't exist - populate from here
+							chunks.slice(chunkIndex, chunks.length - 1).forEach(chunk => {
+								if (settings.arrayNumeric && isFinite(chunk)) {
+									this.$set(node, chunk, []);
+								} else {
+									this.$set(node, chunk, {});
+								}
+								node = node[chunk];
+							});
+							this.$set(node, chunks[chunks.length - 1], value);
+							return true;
+						} else {
+							node = node[chunk];
+							return false;
+						}
+					});
+
+					return value;
+				},
+			}
+		});
 	}
 };
