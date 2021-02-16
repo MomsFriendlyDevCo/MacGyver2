86992fd5d16b2f0bd3b4237511d664413ebd694b
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-02-16 18:19:45 +1100

    Included mgToggle

--- src/components/mgToggle.vue
+++ src/components/mgToggle.vue
@@ -1,50 +1,42 @@
 <script lang="js">
-/*
-// FIXME: Parcel errors with vue-js-toggle-button
-import ToggleButton from 'vue-js-toggle-button';
+import {ToggleButton} from 'vue-js-toggle-button';
+app.component('v-toggle', ToggleButton);
 
-//export default {
-//	install: function(app, options) {
-		app.use(ToggleButton);
-
-		app.mgComponent('mgToggle', {
-			meta: {
-				title: 'Toggle Switch',
-				icon: 'far fa-toggle-on',
-				category: 'Simple Inputs',
-				preferId: true,
-				format: (v, config) => v ? config.onText : config.offText,
-				formatClass: 'text-center',
-				shorthand: ['boolean', 'switch', 'toggle'],
-			},
-			props: {
-				onText: {type: 'mgText', default: 'on'},
-				onColor: {type: 'mgColor', default: '#75c791', advanced: true},
-				offText: {type: 'mgText', default: 'off'},
-				offColor: {type: 'mgColor', default: '#bfcbd9', advanced: true},
-				switchColor: {type: 'mgColor', default: '#fff', advanced: true},
-				disabledColor: {type: 'mgColor', default: '#cccccc', advanced: true},
-			},
-			methods: {
-				changeHandler(e) {
-					this.$mgForm.$emit('mgChange', {path: this.$props.$dataPath, value: e.value});
-				},
-			},
-		});
-	}
-};
-*/
+export default app.mgComponent('mgToggle', {
+	meta: {
+		title: 'Toggle Switch',
+		icon: 'far fa-toggle-on',
+		category: 'Simple Inputs',
+		preferId: true,
+		format: (v, config) => v ? config.onText : config.offText,
+		formatClass: 'text-center',
+		shorthand: ['boolean', 'switch', 'toggle'],
+	},
+	props: {
+		onText: {type: 'mgText', default: 'on'},
+		onColor: {type: 'mgColor', default: '#75c791', advanced: true},
+		offText: {type: 'mgText', default: 'off'},
+		offColor: {type: 'mgColor', default: '#bfcbd9', advanced: true},
+		switchColor: {type: 'mgColor', default: '#fff', advanced: true},
+		disabledColor: {type: 'mgColor', default: '#cccccc', advanced: true},
+	},
+	methods: {
+		changeHandler(e) {
+			this.$mgForm.$emit('mgChange', {path: this.$props.$dataPath, value: e.value});
+		},
+	},
+});
 </script>
 
 <template>
-	<!--toggle-button
+	<v-toggle
 		class="mg-toggle"
 		:value="data"
 		:color="{checked: $props.onColor, unchecked: $props.offColor, disabled: $props.disabledColor}"
 		:labels="{checked: $props.onText, unchecked: $props.offText}"
 		:switchColor="$props.switchColor"
 		@change="changeHandler"
-	/-->
+	/>
 </template>
 
 <style>
