99065c6642c12373a92c112ad8303ab5dd6803b9
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-02-22 18:30:00 +1100

    FEAT: Disabled/Readonly for mgTextArea

--- src/components/mgTextArea.vue
+++ src/components/mgTextArea.vue
@@ -12,6 +12,8 @@ export default app.mgComponent('mgTextArea', {
 		lengthMax: {type: 'mgNumber', title: 'Maximum Length'},
 		placeholder: {type: 'mgTextArea', help: 'Ghost text to display when there is no value'},
 		required: {type: 'mgToggle', default: false},
+		disabled: {type: 'mgToggle', default: false},
+		readonly: {type: 'mgToggle', default: false},
 	},
 	created() {
 		this.$on('mgValidate', reply => {
@@ -28,6 +30,8 @@ export default app.mgComponent('mgTextArea', {
 		v-model="data"
 		class="form-control"
 		:placeholder="$props.placeholder"
+		:disabled="$props.disabled"
+		:readonly="$props.readonly"
 		:minlength="$props.lengthMin"
 		:maxlength="$props.lengthMin"
 		:rows="$props.rows"
