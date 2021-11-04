fff13c8625a592023926afb3218f99989c442cfb
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-11-04 18:48:26 +1000

    Cleanup

--- src/components/mgWysiwyg.vue
+++ src/components/mgWysiwyg.vue
@@ -1,29 +1,25 @@
 <script lang="js">
 import {VueEditor} from 'vue2-editor';
 
-//export default {
-//	install: function(app, options) {
-		export default app.mgComponent('mgWysiwyg', {
-			meta: {
-				title: 'WYSIWYG editor',
-				icon: 'far fa-atlas',
-				category: 'Complex Inputs',
-				preferId: true,
-				shorthand: ['content'],
-			},
-			components: {VueEditor},
-			props: {
-				placeholder: {type: 'mgText', help: 'Ghost text to display when there is no value'},
-				required: {type: 'mgToggle', default: false},
-			},
-			created() {
-				this.$on('mgValidate', reply => {
-					if (this.$props.required && !this.data) return reply(`${this.$props.title} is required`);
-				});
-			},
+export default app.mgComponent('mgWysiwyg', {
+	meta: {
+		title: 'WYSIWYG editor',
+		icon: 'far fa-atlas',
+		category: 'Complex Inputs',
+		preferId: true,
+		shorthand: ['content'],
+	},
+	components: {VueEditor},
+	props: {
+		placeholder: {type: 'mgText', help: 'Ghost text to display when there is no value'},
+		required: {type: 'mgToggle', default: false},
+	},
+	created() {
+		this.$on('mgValidate', reply => {
+			if (this.$props.required && !this.data) return reply(`${this.$props.title} is required`);
 		});
-//	}
-//};
+	},
+});
 </script>
 
 <template>
